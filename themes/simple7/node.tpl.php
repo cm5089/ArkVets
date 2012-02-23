c4256966cd84598f321cf43c324a5d3438cdba0c
cm5089 <chris.martin@fdsystems.co.uk>
2012-02-23 00:17:29 +0000

    Style changes for font

--- themes/simple7/node.tpl.php
+++ themes/simple7/node.tpl.php
@@ -5,8 +5,6 @@
     <h2><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     
   <?php endif; ?>
-    
-  <em class="submitted"><?php print $date; ?> | <?php print $name; ?></em>
   
   <article <?php print $content_attributes ?>>
   
